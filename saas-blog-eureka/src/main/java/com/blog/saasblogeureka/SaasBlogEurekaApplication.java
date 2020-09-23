64bc581e5b5087cc9624d16a1c5454fb30247607
liuyishan <185953937@qq.com>
2020-09-23 09:12:25 +0800

    [liuyishan] 解决注册中心@EnableEurekaServer注解找不到问题。

--- saas-blog-eureka/src/main/java/com/blog/saasblogeureka/SaasBlogEurekaApplication.java
+++ saas-blog-eureka/src/main/java/com/blog/saasblogeureka/SaasBlogEurekaApplication.java
@@ -2,8 +2,10 @@ package com.blog.saasblogeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SaasBlogEurekaApplication {
 
     public static void main(String[] args) {
